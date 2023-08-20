a012ed23598bcb5662928495d9d487a8fdefe522
Mohammad Al-Nur Sarwer Arif <alnurarif@yahoo.com>
2023-08-20 15:31:47 +0600

    it update the link inside note and update to use

--- src/components/ChildComponent.vue
+++ src/components/ChildComponent.vue
@@ -3,13 +3,10 @@
 import { ref } from 'vue';
 
 defineProps(['message','message_from_child'])
-const emit = defineEmits(['update:message_from_child'])
+defineEmits(['update:message_from_child'])
 
 const customMessage = 'Reply from ChildComponent';
 
-const emitMessageEmitted = () => {
-	emit('update:message_from_child', customMessage)
-}
 
 </script>
 
@@ -18,6 +15,6 @@ const emitMessageEmitted = () => {
 	<div class="p-4 border border-gray-300 rounded mt-8">
 		<h2 class="text-lg font-semibold mb-2">Child Component</h2>
 		<p class="font-semibold">Message from ParentComponent: <span class="text-green-600">{{ message }}</span></p>
-		<button  @click="emitMessageEmitted()"  class="px-4 py-2 mt-2 bg-blue-500 text-white rounded">Emit Message</button>
+		<button  @click="$emit('update:message_from_child', customMessage)"  class="px-4 py-2 mt-2 bg-blue-500 text-white rounded">Emit Message</button>
 	</div>
 </template>
